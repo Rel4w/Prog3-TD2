270d3b4dd0647308e2b4d6584aab8d38c9ae11d7
Rel4w <hei.jonathan.4@gmail.com>
2026-01-06 13:41:01 +0300

    ingredient

--- src/main/java/restaurant/models/Ingredient.java
+++ src/main/java/restaurant/models/Ingredient.java
@@ -1,4 +1,91 @@
 package restaurant.models;
 
+import restaurant.enums.CategoryEnum;
+
 public class Ingredient {
+    private Integer id;
+    private String name;
+    private Double price;
+    private CategoryEnum category;
+    private Integer dishId;
+
+    public Ingredient() {}
+
+    public Ingredient(String name, Double price, CategoryEnum category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Ingredient(Integer id, String name, Double price, CategoryEnum category, Integer dishId) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.dishId = dishId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public CategoryEnum getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEnum category) {
+        this.category = category;
+    }
+
+    public Integer getDishId() {
+        return dishId;
+    }
+
+    public void setDishId(Integer dishId) {
+        this.dishId = dishId;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category=" + category +
+                ", dishId=" + dishId +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Ingredient that = (Ingredient) obj;
+        return name != null && name.equalsIgnoreCase(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.toLowerCase().hashCode() : 0;
+    }
 }
